5182725b037adea98ebde4f02a15e44a8a8c83e9
SaraR-1 <s.romiti01@gmail.com>
2023-06-11 18:33:59 +0000

    add sampling of frames from videos

--- historynerf/pose_estimation/colmap.py
+++ historynerf/pose_estimation/colmap.py
@@ -3,28 +3,10 @@ from historynerf.pose_estimation.configs import COLMAPConfig
 from historynerf.pose_estimation.evaluation import compare_poses
 from pathlib import Path
 import pycolmap
-import random
 import shutil
 
-def undersample_images(
-        image_path: Path, 
-        output_dir: Path,
-        rnd_seed: int, 
-        sample_size: int):
-    '''
-    Unsersample the images in the image list. For sanity check, it saves the new image list in a txt file
-    '''
-    if sample_size:
-        image_list = [str(i.name) for i in Path(image_path).iterdir()]
-        random.Random(rnd_seed).shuffle(image_list)
-        undersample_list = image_list[:sample_size]
-        output_dir = Path(output_dir) / f"seed{rnd_seed}_nsamples{sample_size}"
-        output_dir.mkdir()
+from historynerf.pose_estimation.utils.data_preprocessing import undersample_images, undersample_video_frames, undersample_fromlist
 
-        with open(str(output_dir / f"undersample_list.txt"), "w") as f:
-            f.write("\n".join(undersample_list))
-        return undersample_list, output_dir
-    return [], output_dir
 
 class COLMAPPoseEstimator(PoseEstimator):
     def __init__(
@@ -41,7 +23,8 @@ class COLMAPPoseEstimator(PoseEstimator):
             camera_model = self.config.camera_model,
             image_list = image_list,
             sift_options = {"gpu_index": self.config.use_gpu},
-            verbose = False,
+            device="cuda",
+            verbose = True,
         )
     
     def _match_features(self):
@@ -49,7 +32,9 @@ class COLMAPPoseEstimator(PoseEstimator):
         if self.config.matching_method == "exhaustive":
             pycolmap.match_exhaustive(
                 database_path = self.config.database_path,
-                verbose = False,
+                verbose = True,
+                sift_options = {"gpu_index": self.config.use_gpu},
+                device="cuda",
                 # default values {"max_ratio": 0.8, "max_distance": 0.7, "min_num_inliers": 15}
                 # sift_options = {"max_ratio": 0.8, "max_distance": 0.7, "min_num_inliers": 5},
             )
@@ -57,16 +42,22 @@ class COLMAPPoseEstimator(PoseEstimator):
             pycolmap.match_sequential(
                 database_path = self.config.database_path,
                 verbose = False,
+                sift_options = {"gpu_index": self.config.use_gpu},
+                device="cuda",
             )
         elif self.config.matching_method == "spatial":
             pycolmap.match_spatial(
                 database_path = self.config.database_path,
                 verbose = False,
+                sift_options = {"gpu_index": self.config.use_gpu},
+                device="cuda",
             )
         elif self.config.matching_method == "vocabtree":
             pycolmap.match_vocabtree(
                 database_path = self.config.database_path,
                 verbose = False,
+                sift_options = {"gpu_index": self.config.use_gpu},
+                device="cuda",
             )
         else:
             raise ValueError(f"Matching method {self.config.matching_method} not supported")
@@ -79,20 +70,29 @@ class COLMAPPoseEstimator(PoseEstimator):
             output_path = self.config.output_dir, )
 
     def estimate_poses(self):
+        # Init empty list of images to undersample and output directory
+        undersample_image_list = []
+        output_dir = self.config.output_dir
+
         if self.config.image_list:
-            output_dir = Path(self.config.output_dir) / f"chosen_nsamples{len(self.config.image_list)}"
-            if output_dir.exists():
-                output_dir = Path(self.config.output_dir) / f"chosen_nsamples{len(self.config.image_list)}_{self.config.seed}"
-            output_dir.mkdir()
-            with open(str(output_dir / f"undersample_list.txt"), "w") as f:
-                f.write("\n".join(self.config.image_list))
-            undersample_image_list = self.config.image_list
-        else:    
+            undersample_image_list, output_dir = undersample_fromlist(
+                output_dir=self.config.output_dir,
+                image_list=self.config.image_list,
+                rnd_seed=self.config.seed,)
+            
+        elif self.config.sample_size:    
             undersample_image_list, output_dir = undersample_images(
                 image_path=self.config.image_dir,
                 output_dir=self.config.output_dir, 
                 rnd_seed=self.config.seed, 
                 sample_size=self.config.sample_size)
+        
+        elif self.config.video_sample_step:
+            undersample_image_list, output_dir = undersample_video_frames(
+                video_path=self.config.video_path,
+                output_dir=self.config.output_dir,
+                step_size=self.config.video_sample_step,)
+            
         self.config.output_dir = output_dir
 
         self._extract_features(image_list = undersample_image_list)
--- historynerf/pose_estimation/configs.py
+++ historynerf/pose_estimation/configs.py
@@ -14,21 +14,31 @@ class COLMAPConfig(PoseEstimationConfig):
     camera_model: str = "SIMPLE_RADIAL"
     use_gpu: str = 7
     matching_method: str = "exhaustive"
-    seed: int = 0
-    sample_size: Optional[int] = None
-    image_list: Optional[List[str]] = None
     # For evaluation purposes, path to the ground truth poses
     gt_poses_dir: Optional[str] = None
     angular_error: Optional[int] = None
     translation_error: Optional[float] = None
+    # TODO: move them to have a data preprocessing separate config
+    seed: int = 0
+    sample_size: Optional[int] = None
+    image_list: Optional[List[str]] = None
+    video_sample_step: Optional[int] = None
 
 @dataclass
 class OpenMVGConfig(PoseEstimationConfig):
     camera_model: str = "TEST"
 
 @dataclass
+class DataConfig:
+    seed: int = 0
+    sample_size: Optional[int] = None
+    image_list: Optional[List[str]] = None
+    video_sample_step: Optional[int] = None
+
+@dataclass
 class Config:
     pose_config: PoseEstimationConfig = MISSING
+    # data_config: DataConfig = MISSING
     wandb_project: str = MISSING
     wandb_log: bool = True
 
