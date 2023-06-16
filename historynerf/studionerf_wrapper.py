3ddda2c9834d070661fdb9ea7b818a25fdee5b21
SaraR-1 <s.romiti01@gmail.com>
2023-06-16 11:58:10 +0000

    initial version nerfstudio wrapper

--- historynerf/studionerf_wrapper.py
+++ historynerf/studionerf_wrapper.py
@@ -1,7 +1,22 @@
 import os
 
+from historynerf.config import PoseEstimationConfig, NeRFConfig
+
+def argument_name_parser(argument):
+    return argument.replace("_", "-")
+
+def flag_to_argument(flag_name, flag_value):
+    return flag_name if flag_value else f"no-{flag_name}"
+
 class NSWrapper:
-    def __init__(self):
+    def __init__(
+            self,
+            preprocessing_config: PoseEstimationConfig,
+            nerf_config: NeRFConfig):
+        
+        self.preprocessing_config = preprocessing_config
+        self.nerf_config = nerf_config
+
         self.initialize()
     
     def initialize(self):
@@ -12,10 +27,19 @@ class NSWrapper:
 
     def process_data(self):
         '''
-        Data Preprocessing (includes pose estimation with COLMAP)
+        Process images into a nerfstudio dataset.
+        1. Scales images to a specified size.
+        2. Calculates the camera poses for each image using `COLMAP <https://colmap.github.io/>`_.
+
+        Run example:
+        ns-process-data images --data ../data/bridge_of_sighs/output_temp/every5frames/ --output-dir ../data/bridge_of_sighs/output_temp/processed_data
         '''
+        # Parse all the arguments names, replace "_" with "-"
+
         # TODO: change as needed
-        command = f"ns-process-data {images, video} --data {DATA_PATH} --output-dir {PROCESSED_DATA_DIR}"
+        # 83 images, started at 12.38 - finished at ??.??
+        command = f"ns-process-data images --data {DATA_PATH} --output-dir {PROCESSED_DATA_DIR}"
+
         os.system(command)
 
     def train(self):
